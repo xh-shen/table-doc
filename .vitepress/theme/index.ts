53ac88d4eaff8b75e6fb3048b77904baf92defe9
shen-web <9509394@qq.com>
2022-12-04 12:35:16 +0800

    chore: update @shene/table version

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,9 +2,10 @@
  * @Author: shen
  * @Date: 2022-12-02 15:46:07
  * @LastEditors: shen
- * @LastEditTime: 2022-12-04 10:30:36
+ * @LastEditTime: 2022-12-04 12:14:50
  * @Description:
  */
+import '@shene/table/dist/index.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './styles/fonts.css'
@@ -15,17 +16,17 @@ import './styles/components/custom-block.css'
 import './styles/components/vp-code.css'
 import './styles/components/vp-doc.css'
 import './styles/components/vp-sponsor.css'
-import '@shene/table/dist/index.css'
 import './styles/element-vars.css'
 
-import * as STable from '@shene/table'
+import STable, { setLicenseKey } from '@shene/table'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
-import { globals } from '../components'
-import { Theme } from 'vitepress'
 import VPBadge from './components/VPBadge.vue'
 import Layout from './Layout.vue'
 import NotFound from './NotFound.vue'
+import { globals } from '../components'
+
+import type { Theme } from 'vitepress'
 
 export { default as VPHomeHero } from './components/VPHomeHero.vue'
 export { default as VPHomeFeatures } from './components/VPHomeFeatures.vue'
@@ -36,7 +37,6 @@ export { default as VPTeamPageTitle } from './components/VPTeamPageTitle.vue'
 export { default as VPTeamPageSection } from './components/VPTeamPageSection.vue'
 export { default as VPTeamMembers } from './components/VPTeamMembers.vue'
 
-const { setLicenseKey } = STable
 setLicenseKey('95cc65b4d02a57205c17469dc1a32455RE9NQUlOPXNoZW5lLm9yZy5jbixFWFBJUlk9MTk4NTYxNjAwMDAwMA==')
 
 const theme: Theme = {
@@ -47,7 +47,7 @@ const theme: Theme = {
 		app.use(ElementPlus as any, {
 			locale: zhCn
 		})
-		app.use(STable.default)
+		app.use(STable)
 		globals.forEach(([name, Comp]) => {
 			app.component(name, Comp as any)
 		})
--- prod.sh
+++ prod.sh
@@ -1,4 +1,5 @@
 git pull origin dev
 npm install --registry=https://registry.npm.taobao.org
+npm install @shene/table
 npm run build
 rm -rf /www/nginx/html/table-doc-html/ && mv ./.vitepress/dist/ /www/nginx/html/table-doc-html/
