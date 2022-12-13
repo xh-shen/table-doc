530ffca3a528748ff97da039760aa75952e51a94
shen-web <9509394@qq.com>
2022-12-13 16:56:39 +0800

    feat(examples): add ellipsis exmples

---
+++ examples/basic/ellipsis.vue
@@ -0,0 +1,100 @@
+<template>
+	<s-table :columns="columns" :data-source="dataSource" :pagination="false">
+		<template #bodyCell="{ text, column, record }">
+			<template v-if="column.key === 'name'">
+				<a>{{ text }}</a>
+			</template>
+			<template v-else-if="column.key === 'tags'">
+				<ElSpace>
+					<ElTag v-for="item in record.tags" :key="item" :type="item === '前端' ? 'danger' : item === '项目' ? '' : 'success'">
+						{{ item }}
+					</ElTag>
+				</ElSpace>
+			</template>
+			<template v-else-if="column.key === 'action'">
+				<ElSpace size="large">
+					<a>编辑</a>
+					<a>删除</a>
+				</ElSpace>
+			</template>
+		</template>
+	</s-table>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { ElTag, ElSpace } from 'element-plus'
+import type { STableColumnsType } from '@shene/table'
+
+interface DataType {
+	key: string
+	name: string
+	age: number
+	sex: string
+	date: string
+	address: string
+	tags: string[]
+}
+
+const columns: STableColumnsType<DataType> = [
+	{
+		title: '姓名(数据需要长一些才能生效)',
+		dataIndex: 'name',
+		key: 'name',
+		width: 120,
+		ellipsis: true,
+		ellipsisTitle: { showTitle: false }
+	},
+	{
+		title: '年龄',
+		dataIndex: 'age',
+		key: 'age',
+		width: 100
+	},
+	{
+		title: '性别',
+		dataIndex: 'sex',
+		key: 'sex',
+		width: 100
+	},
+
+	{
+		title: '职业(我应该是一个很长很长很长的标题)',
+		key: 'tags',
+		dataIndex: 'tags',
+		width: 220,
+		ellipsisTitle: true
+	},
+	{
+		title: '日期',
+		dataIndex: 'date',
+		key: 'date',
+		width: 100,
+		ellipsis: { showTitle: false }
+	},
+	{
+		title: '地址',
+		dataIndex: 'address',
+		key: 'address'
+	},
+	{
+		title: '操作',
+		key: 'action',
+		width: 120
+	}
+]
+
+const data: DataType[] = []
+for (let i = 0; i < 5; i++) {
+	data.push({
+		key: i.toString(),
+		name: ['张三', '李四', '王五', '马六'][i % 4] + '(数据需要长一些才能生效)',
+		age: [18, 30, 26, 45][i % 4],
+		sex: ['男', '女'][i % 2],
+		date: '2022-12-12 08:00:00',
+		address: ['北京', '上海', '天津', '重庆'][i % 4] + '市某某区某某大街520号',
+		tags: [['前端', '后端'], ['后端'], ['前端', '产品', '项目'], ['测试']][i % 4]
+	})
+}
+
+const dataSource = ref(data)
+</script>
